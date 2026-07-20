ec38af08d73e9d1354f01af0a1fe79c58db717c2
gooze <gooze@work>
2026-07-20 07:54:58 +0200

    Markdown stuff

--- dot_config/nvim/lua/plugins/markdown.lua
+++ dot_config/nvim/lua/plugins/markdown.lua
@@ -1,5 +1,32 @@
 return {
-  "MeanderingProgrammer/render-markdown.nvim",
-  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" },
-  opts = {},
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" },
+    opts = {},
+  },
+  {
+    "Kicamon/markdown-table-mode.nvim",
+    config = function()
+      require("markdown-table-mode").setup()
+    end,
+  },
+  {
+    "roodolv/markdown-toggle.nvim",
+    config = function()
+      require("markdown-toggle").setup({
+        keymaps = {
+          toggle = {
+            ["<C-q>"] = "quote",
+            ["<C-l>"] = "list",
+            ["<Leader><C-l>"] = "list_cycle",
+            ["<C-n>"] = "olist",
+            ["<M-x>"] = "checkbox",
+            ["<Leader><M-x>"] = "checkbox_cycle",
+            ["<C-h>"] = "heading",
+            ["<Leader><C-h>"] = "heading_toggle",
+          },
+        },
+      })
+    end,
+  },
 }
