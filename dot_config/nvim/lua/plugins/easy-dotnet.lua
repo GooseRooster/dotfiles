66d2bd0bdb2bd459f65f934617150211a8624e95
gooze <gooze@work>
2026-07-29 14:33:05 +0200

    try again again

--- dot_config/nvim/lua/plugins/easy-dotnet.lua
+++ dot_config/nvim/lua/plugins/easy-dotnet.lua
@@ -183,24 +183,6 @@ return {
 					end
 				end,
 			})
-
-			require("conform").setup({
-				formatters_by_ft = {
-					razor = { "cleanupcode" },
-					cshtml = { "cleanupcode" },
-					cs = { "cleanupcode" },
-				},
-				formatters = {
-					cleanupcode = {
-						command = "jb",
-						args = { "cleanupcode", "--profile=Built-in: Reformat Code", "$FILENAME" },
-						stdin = false,
-					},
-				},
-				format_on_save = {
-					lsp_fallback = true,
-				},
-			})
 		end,
 	},
 }
