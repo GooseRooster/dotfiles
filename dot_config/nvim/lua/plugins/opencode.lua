e2dbae36f848ad8ad44b8ea81c867240df17e33b
gooze <gooze@work>
2026-08-09 13:15:58 +0200

    changed some stuff

--- dot_config/nvim/lua/plugins/opencode.lua
+++ dot_config/nvim/lua/plugins/opencode.lua
@@ -1,7 +1,7 @@
 return {
 	"nickjvandyke/opencode.nvim",
 	version = "*", -- Latest stable release
-	enabled = false, -- disabled for now.
+	enabled = true,
 	config = function()
 		---@type opencode.Opts
 		vim.g.opencode_opts = {
