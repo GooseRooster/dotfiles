1ab5baf57cfed279ad1def9395be01ef6470850e
gooze <gooze@bluefin>
2026-08-10 19:20:33 +0200

    Update .config/nvim/lua/plugins/dashboard.lua

--- dot_config/nvim/lua/plugins/dashboard.lua
+++ dot_config/nvim/lua/plugins/dashboard.lua
@@ -1,10 +1,10 @@
 return {
-	{
-		"folke/snacks.nvim",
-		opts = {
-			dashboard = {
-				preset = {
-					header = [[ 
+  {
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          header = [[ 
                                      
            =.            =           
          ===--           ===         
@@ -23,8 +23,17 @@ return {
            *            :*           
                                      
           ]],
-				},
-			},
-		},
-	},
+          -- stylua: ignore
+       ---@type snacks.dashboard.Item[]
+       keys = {
+         { icon = " ", key = "p", desc = "Projects", action = ":lua Snacks.picker.projects()" },
+         { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+         { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+         { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+         { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+       },
+        },
+      },
+    },
+  },
 }
