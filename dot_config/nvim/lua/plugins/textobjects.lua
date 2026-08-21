68e9bf8f3e1ff750567f0167c6ff254c4202db36
gooze <gooze@work>
2026-08-21 15:22:25 +0200

    textobjects

---
+++ dot_config/nvim/lua/plugins/textobjects.lua
@@ -0,0 +1,15 @@
+-- lua/plugins/textobjects.lua
+return {
+	{
+		"nvim-mini/mini.ai",
+		opts = function(_, opts)
+			local ai = require("mini.ai")
+			opts.custom_textobjects = opts.custom_textobjects or {}
+			opts.custom_textobjects.x = ai.gen_spec.treesitter({
+				a = "@attribute.outer",
+				i = "@attribute.inner",
+			})
+			return opts
+		end,
+	},
+}
